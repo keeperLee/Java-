b2b2ab2178fb5dff7489e4bf8bef0aa4e81ade7b
keeperlee <leejamesjian@163.com>
2018-11-02 10:45:24 +0800

    主线程调用客户线程的join()方法，等到客户线程运行结束才恢复运行。

---
+++ src/com/thread/com/JoinThread.java
@@ -0,0 +1,25 @@
+package com.thread.com;
+
+public class JoinThread {
+	public static void main(String[] args) {
+		MyThread5 mt = new MyThread5();
+		mt.start();
+		try {
+			mt.join();					//挂起主线程，等待客户线程执行结束
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		for(int i=1;i<=5;i++){
+			System.out.println(Thread.currentThread().getName());
+		}
+	}
+}
+
+class MyThread5 extends Thread{
+	@Override
+	public void run() {
+		for(int i=1;i<=5;i++){
+			System.out.println(currentThread().getName());
+		}
+	}
+}
